0a3b483918df495fc5ca5f1a19a929801f7bb11c
milos381 <milos_popovic@outlook.com>
2021-01-02 15:28:47 -0500

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let heartChange = {
+  "♡": "♥",
+  "♥": "♡"
+};
+
+let colorChange = {
+  "red" : "",
+  "": "red"
+};
+let like = document.querySelectorAll(".like-glyph");
+let errorMsg = document.getElementById("modal");
+errorMsg.classList.add("hidden");
+function errorFn() {
+  errorMsg.classList.remove("hidden");
+}
 
 
+for (var i=0;i<like.length;i++) {
+  like[i].addEventListener("click", function (e) {
+    mimicServerCall("something")
+    .then(function(serverMessage){
+      e.target.innerText = heartChange[e.target.innerText]
+      e.target.style.color = colorChange[e.target.style.color]
+   })
+   .catch(function(error) {
+      setTimeout(errorFn(), 5000);
+  })
+  });
+}
 
 
 //------------------------------------------------------------------------------
